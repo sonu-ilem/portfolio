428938d05e075e21fa127366ec301bbb1d34ce4f
Sonu Kumar <sksonu8544@gmail.com>
2024-05-19 11:14:30 +0530

    setough portfolio using next js

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import ProjectSection from "@/components/ProjectSection";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-center text-3xl" >Navigation And Home banner</h1>
+      <ProjectSection />
+    </>
+  );
+}
---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProjectSection() {
+    return (
+        <>
+            <h1>project</h1>
+        </>
+    )
+}
